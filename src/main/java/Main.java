54b72a451bbdedab7b1e1541ebac63f6d7c88cc2
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 08:18:13 -0400

    V1 C

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter the number of the month. ");
+        int month = input.nextInt();
+        System.out.print("The name of the month is ");
+
+        String name;
+        switch(month) {
+            case 1: name = "January.";
+                break;
+            case 2: name = "February.";
+                break;
+            case 3: name = "March.";
+                break;
+            case 4: name = "April.";
+                break;
+            case 5: name = "May.";
+                break;
+            case 6: name = "June.";
+                break;
+            case 7: name = "July.";
+                break;
+            case 8: name = "August.";
+                break;
+            case 9: name = "September.";
+                break;
+            case 10: name = "October.";
+                break;
+            case 11: name = "November.";
+                break;
+            case 12: name = "December.";
+                break;
+            default: name = "ERROR.";
+        }
+        System.out.printf("%s", name);
 
     }
 }
